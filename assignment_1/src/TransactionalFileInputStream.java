a4ff7d94d62fa8f3ba6782f97f8b58df4ab26360
scriptkid85 <wgiveny@gmail.com>
2013-01-23 21:58:26 +0800

    basic frame for assignment 1

---
+++ assignment_1/src/TransactionalFileInputStream.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+
+public class TransactionalFileInputStream extends InputStream implements Serializable{
+
+  @Override
+  public int read() throws IOException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+
+}
---
+++ assignment_1/src/TransactionalFileOutputStream.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class TransactionalFileOutputStream extends OutputStream implements Serializable{
+
+  @Override
+  public void write(int arg0) throws IOException {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
