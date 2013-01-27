b649d4f3b3963fc249b8b2bc3c5f24248e1658e5
scriptkid85 <wgiveny@gmail.com>
2013-01-27 17:34:51 +0800

    finish local ProcessManager

--- assignment_1/src/ProcessManager.java
+++ assignment_1/src/ProcessManager.java
@@ -1,22 +1,45 @@
 import java.io.*;
 import java.lang.reflect.*;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.StringTokenizer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class ProcessManager {
 
-  private static ArrayList<String> running_process;
+  private static Hashtable<Thread, String> process_table;
 
   public static void Ps() {
-    if (running_process.size() < 1) {
+    if (process_table.size() < 1) {
       System.out.println("no running processes");
+      System.out.flush();
       return;
     }
-    for (String process : running_process) {
-      System.out.println(process);
+    for (Thread t: process_table.keySet()) {
+      System.out.println(process_table.get(t));
+      System.out.flush();
     }
   }
 
+  private static void updateProcessList(){
+    Hashtable<Thread, String> tempProcesstable = new Hashtable<Thread, String>();
+    for(Thread t: process_table.keySet()){
+      if(!t.isAlive()){
+        System.out.println("Process " + process_table.get(t) +  " was terminated");
+        System.out.flush();
+               
+      }
+      else{
+        tempProcesstable.put(t, process_table.get(t));
+      }
+    }
+    process_table.clear();
+    process_table = tempProcesstable;
+  }
+  
   public static void Runprocess(String command) throws ClassNotFoundException, Exception {
     StringTokenizer stoken = new StringTokenizer(command);
     String cmd = stoken.nextToken();
@@ -29,7 +52,9 @@ public class ProcessManager {
       Constructor[] ProcessCtor = ProcessClass.getConstructors();
       Object Cmdprocess = ProcessCtor[0].newInstance((Object) command.split(" "));
       Thread t = new Thread((Runnable) Cmdprocess);
-      running_process.add(cmd + " " + arguments);
+      if(process_table.containsKey(t))
+        process_table.remove(t);
+      process_table.put(t, cmd + " " + arguments);
       t.start();
     } catch (ClassNotFoundException e) {
       System.out.println("Cannot find the input command.");
@@ -63,7 +88,16 @@ public class ProcessManager {
 
     String commandline;
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    running_process = new ArrayList<String>();
+    process_table = new Hashtable<Thread, String>();
+    
+    ScheduledExecutorService schExec = Executors.newScheduledThreadPool(8);
+    ScheduledFuture<?> schFuture = schExec.scheduleWithFixedDelay(new Runnable() {
+      @Override
+      public void run() {
+        updateProcessList();
+        return;
+      }
+    }, 0, 5, TimeUnit.SECONDS);
     
     while (true) {
       System.out.println("==>");
