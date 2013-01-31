bebe200ffcb1a170e43c087d88b5ebd55a9d0b5c
Zeyuan Li <zyli5313@gmail.com>
2013-01-31 14:10:57 -0500

    add byte serialize function to Serializer

--- assignment_1/src/Serializer.java
+++ assignment_1/src/Serializer.java
@@ -1,9 +1,13 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.UUID;
 
 /**
@@ -12,9 +16,64 @@ import java.util.UUID;
  * @author Zeyuan Li
  * */
 public class Serializer {
-  // TODO: this path need to be on afs so that multiple processes can access
   private String pathPrefix = ""; // afs string
+  
+  public byte[] serializeBytes(MigratableProcess mp) {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    ObjectOutput out = null;
+    byte[] outbytes = null;
+    
+    System.out.println("Byte Serializer: start");
+    
+    // suspend process
+    mp.suspend();
+    
+    try {
+      out = new ObjectOutputStream(bos);   
+      out.writeObject(mp);
+      out.flush();
+      outbytes = bos.toByteArray();
+      
+      out.close(); 
+ 
+    } catch (FileNotFoundException e1) {
+      System.err.println(e1);
+      e1.printStackTrace();
+    } catch (IOException e1) {
+      System.err.println(e1);
+      e1.printStackTrace();
+    }
+    return outbytes;
+  }
+  
+  public MigratableProcess deserializeBytes(byte[] objbytes) {
+    MigratableProcess mp = null;
+    ByteArrayInputStream bis = new ByteArrayInputStream(objbytes);
+    ObjectInput in = null;
 
+    // if it resumes running, read object in
+    if (objbytes != null) {
+      try {
+        in = new ObjectInputStream(bis);
+        mp = (MigratableProcess) in.readObject(); 
+        
+      } catch (IOException e) {
+        System.err.println("Deserialize IOException");
+        e.printStackTrace();
+      } catch (ClassNotFoundException e) {
+        System.err.println("Deserialize ClassNotFoundException ");
+        e.printStackTrace();
+      }
+    }
+    else {
+      System.out.println("Deserialize failed: obj byte array null");
+    }
+    if(mp == null)
+      System.out.println("Deserialized result null");
+    return mp;
+  }
+
+  // TODO: serialize serializable object
   public String serialize(MigratableProcess mp) {
     System.out.println("Serializer: start");
     String id = UUID.randomUUID().toString();
